78284a670a4a297363db81cecc7dc4edce71796f
JustABlub <minhanpark882@gmail.com>
2024-12-09 15:36:16 -0600

    did some work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+use std::thread::sleep;
+use std::time::{Duration, Instant};
+use chrono::{DateTime, Local, Duration};
+use std::cmp::Ordering;
+
+struct Clock {
+    start: DateTime<Local>,
+    duration: u32,
+    end: DateTime<Local>,
+}
+
 fn install_client() {
     let na_client_url = "https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.na.exe";
     let response = reqwest::blocking::get(na_client_url)
@@ -7,10 +19,57 @@ fn install_client() {
 
     std::process::Command::new(r".\LoLClientInstaller.exe")
         .output()
-        .expect("failed to execute process");
+        .expect("Failed to execute process");
 }
 
+fn set_clock() -> Clock {
+    let mut start_time = chrono::Local::now();
+
+    println!("Enter clock duration in minutes: ");
+    let mut duration = String::new();
+    io::stdin()
+        .read_line(&mut duration)
+        .expect("Failed to read line");
+    let duration: u32 = duration.trim().parse().expect("Invalid number");
+    let end_time = start_time + Duration::minutes(duration as i64);
+
+    let clock = Clock{
+        start: start_time,
+        duration,
+        end: end_time,
+    };
+
+    let formatted_start_time = start_time.format("%Y-%m-%d %H:%M:%S").to_string();
+    let formatted_end_time = end_time.format("%Y-%m-%d %H:%M:%S").to_string();
+    println!("Clock set. Start time: {:?}, End time: {:?}", formatted_start_time, formatted_end_time);
+
+    return clock;
+}
+
+fn check_clock(clock: Clock) -> bool {
+    match clock.end(&chrono::Local::now()) {
+        Ordering::Less => return False,
+        Ordering::Greater => return True,
+        Ordering::Equal => return True,
+    }
+}
+
+fn uninstall_league() {
+
+}
 
 fn main() {
-    install_client();
+    //install_client();
+    clock = set_clock();
+
+    let interval = Duration::from_secs(5);
+    let mut next_time = Instant::now() + interval;
+    loop {
+        if check_clock(clock) {
+            break;
+        }
+        sleep(next_time - Instant::now());
+        next_time += interval;
+    }
+    uninstall_league();
 }
