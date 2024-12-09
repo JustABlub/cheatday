11a7f2817324a34a43a87916588e394532d9b99a
JustABlub <minhanpark882@gmail.com>
2024-12-08 23:50:04 -0600

    Added way to run client installer after downloading.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cheatday
 Safely scratch the itch of wanting to play league without the risk of addiction.
+Written in rust instead of powershell because I'm stupid.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
-fn main() {
+fn install_client() {
     let na_client_url = "https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.na.exe";
     let response = reqwest::blocking::get(na_client_url)
         .expect("Failed to download installer");
     let client = response.bytes().expect("response invalid");
-    let _ = std::fs::write("lolinstall.exe", &client);
+    let _ = std::fs::write("LoLClientInstaller", &client);
+
+    std::process::Command::new("LoLClientInstaller.exe")
+        .output()
+        .expect("failed to execute process");
+}
+
+
+fn main() {
+    install_client();
 }
