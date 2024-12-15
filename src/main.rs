c7b4fccade164ed6df1cd18c2737502f7e0aaa42
JustABlub <minhanpark882@gmail.com>
2024-12-15 17:14:47 -0600

    Working on winstreak mode

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
 use std::io;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
+use std::collections::HashMap;
 use chrono::{DateTime, Local, Duration as ChrDuration};
 use std::cmp::Ordering;
+use clap::Parser;
 
 struct Clock {
     start: DateTime<Local>,
     duration: u32,
     end: DateTime<Local>,
+}
+
+/// Scratching the league of legends itch without risking addiction
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None, arg_required_else_help(true))]
+
+struct Args {
+    /// Countdown mode
+    #[arg(short, long, value_name("MINUTES"), exclusive(true))]
+    countdown: Option<u8>,
+
+    /// Winstreak mode
+    #[arg(short, long, value_name("\"USER#TAG\""), exclusive(true))]
+    winstreak: Option<String>,
 }
 
 fn install_client() {
@@ -55,22 +71,8 @@ fn check_clock(clock: &Clock) -> bool {
     }
 }
 
-fn uninstall_league() {
-    /* Needs to turn off riot client for it to work
-    std::process::Command::new(r"C:\Riot Games\Riot Client\RiotClientServices.exe")
-        .arg("--uninstall-product=league_of_legends")
-        .arg("--uninstall-patchline=live")
-        .spawn()
-        .expect("Failed to uninstall");
-    */
-    // nuclear option
-    let _= std::fs::remove_dir_all(r"C:\Riot Games\League of Legends");
-}
-
-fn main() {
-    install_client();
+fn countdown_mode() {
     let mut clock = set_clock();
-
     let interval = Duration::from_secs(1);
     let mut next_time = Instant::now() + interval;
     loop {
@@ -81,4 +83,34 @@ fn main() {
         next_time += interval;
     }
     uninstall_league();
+}
+
+fn winstreak_mode() {
+    let api_key: String = std::env::var("RIOT_API").expect("API key not found");
+    // parse args.winstreak to get user and tag seperate then plug into format line
+    let api_url = format!("https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/just%20a%20tako/3444?api_key={}", api_key);
+    let puuid = reqwest::blocking::get(api_url)
+        .unwrap()
+        .json::<HashMap<String, String>>()
+        .unwrap();
+    println!("{:#?}", puuid);
+}
+
+fn uninstall_league() {
+    std::process::Command::new(r"C:\Riot Games\Riot Client\RiotClientServices.exe")
+        .arg("--uninstall-product=league_of_legends")
+        .arg("--uninstall-patchline=live")
+        .spawn()
+        .expect("Failed to uninstall");
+
+    // optional nuclear option
+    // let _= std::fs::remove_dir_all(r"C:\Riot Games\League of Legends");
+}
+
+fn main() {
+    let args = Args::parse();
+    //install_client();
+    if args.countdown != None { countdown_mode() }
+    else if args.winstreak != None { winstreak_mode() }
+
 }
